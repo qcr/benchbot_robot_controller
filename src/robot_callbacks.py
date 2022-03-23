d98de48a8cb5a0e684e70e7012ad5120602039c5
Ben Talbot <b.talbot@qut.edu.au>
2022-03-23 16:45:26 +0000

    Remove manual spatial geometry functions

--- src/robot_callbacks.py
+++ src/robot_callbacks.py
@@ -20,91 +20,12 @@ _MOVE_POSE_K_ALPHA = 7.5
 _MOVE_POSE_K_BETA = -3
 
 
-def __ang_to_b(matrix_a, matrix_b):
-    # Computes the angle to matrix b, from the point specified in matrix a
-    return np.arctan2(matrix_b[1, 3] - matrix_a[1, 3],
-                      matrix_b[0, 3] - matrix_a[0, 3])
-
-
-def __ang_to_b_wrt_a(matrix_a, matrix_b):
-    # Computes the 2D angle to the location of homogenous transformation matrix
-    # b, wrt a
-    diff = __tr_b_wrt_a(matrix_a, matrix_b)
-    return np.arctan2(diff[1, 3], diff[0, 3])
-
-
-def __dist_from_a_to_b(matrix_a, matrix_b):
-    # Computes 2D distance from homogenous transformation matrix a to b
-    return np.linalg.norm(__tr_b_wrt_a(matrix_a, matrix_b)[0:2, 3])
-
-
 def __pi_wrap(angle):
     return np.mod(angle + np.pi, 2 * np.pi) - np.pi
 
 
-def __pose_vector_to_tf_matrix(pv):
-    # Expected format is [x,y,z,w,X,Y,Z]
-    return np.vstack((np.hstack((Rot.from_quat(pv[0:4]).as_dcm(),
-                                 np.array(pv[4:]).reshape(3,
-                                                          1))), [0, 0, 0, 1]))
-
-
 def __safe_dict_get(d, key, default):
     return d[key] if type(d) is dict and key in d else default
-
-
-def __tr_b_wrt_a(matrix_a, matrix_b):
-    # Computes matrix_b - matrix_a (transform b wrt to transform a)
-    return np.matmul(np.linalg.inv(matrix_a), matrix_b)
-
-
-def __transrpy_to_tf_matrix(trans, rpy):
-    # Takes a translation vector & roll pitch yaw vector
-    return __pose_vector_to_tf_matrix(
-        np.hstack((Rot.from_euler('XYZ', rpy).as_quat(), trans)))
-
-
-def __yaw(matrix):
-    # Extracts the yaw value from a matrix
-    return Rot.from_dcm(matrix[0:3, 0:3]).as_euler('XYZ')[2]
-
-
-def __yaw_b_wrt_a(matrix_a, matrix_b):
-    # Computes the yaw diff of homogenous transformation matrix b w.r.t. a
-    return Rot.from_dcm(__tr_b_wrt_a(matrix_a,
-                                     matrix_b)[0:3, 0:3]).as_euler('XYZ')[2]
-
-
-def _debug_move(data, publisher, controller):
-    # Accepts:
-    # - rot_yaw: forms a tf matrix using yaw alone
-    # - rot_xyzw: forms a tf matrix using xyzq quat
-    # - trans_xyz: forms a tf matrix using xyz translation
-    # Priority:
-    # - Uses 'rot_yaw' if both rotation options are provided
-    # Default:
-    # - Uses 0 rotation & translation if values are missing
-    def print_pose(pose):
-        rpy = Rot.from_dcm(pose[0:3, 0:3]).as_euler('XYZ', degrees=True)
-        return ("rpy: %f, %f, %f  xyz: %f, %f, %f" %
-                tuple(np.hstack((rpy, pose[0:3, 3].transpose()))))
-
-    pose_a = _current_pose(controller)
-    print("STARTING @ POSE: %s" % print_pose(pose_a))
-    relative_pose = (__transrpy_to_tf_matrix(
-        __safe_dict_get(data, 'trans_xyz', [0, 0, 0]),
-        [0, 0, np.deg2rad(__safe_dict_get(data, 'rot_yaw', 0))])
-                     if 'rot_yaw' in data else __pose_vector_to_tf_matrix(
-                         __safe_dict_get(data, 'rot_xyzw', [0, 0, 0, 1]) +
-                         __safe_dict_get(data, 'trans_xyz', [0, 0, 0])))
-    print("GOAL POSE (RELATIVE): %s" % print_pose(relative_pose))
-    print("GOAL POSE (ABSOLUTE): %s" %
-          print_pose(np.matmul(pose_a, relative_pose)))
-    _move_to_pose(np.matmul(pose_a, relative_pose), publisher, controller)
-    pose_b = _current_pose(controller)
-    print("FINAL POSE (RELATIVE): %s" %
-          print_pose(__tr_b_wrt_a(pose_a, pose_b)))
-    print("FINAL POSE (ABSOLUTE): %s" % print_pose(pose_b))
 
 
 def _define_initial_pose(controller):
