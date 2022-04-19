5f09ab9f0f1052053a76a4eb81abdb369d64dd6f
root <b.talbot@qut.edu.au>
2022-04-19 05:52:46 +0000

    Correctly declare benchbot_spatial submodule
    Addresses qcr/benchbot#55.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['benchbot_robot_controller, benchbot_spatial'],
+    packages=['benchbot_robot_controller', 'benchbot_spatial'],
     package_dir={'': 'src'})
 
 setup(**d)
